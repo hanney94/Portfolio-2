05626d6dbf82ae5a8b3323cd91bd34e76a87ccfe
hanney94 <hannahallan94@gmail.com>
2024-09-10 19:33:57 +0100

    Added JS

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.contact-form').addEventListener('submit', function(e) {
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        if (!name || !email || !message) {
+            alert('Please fill out all fields.');
+            e.preventDefault(); // Prevent form submission if fields are missing
+        }
+    });
+
